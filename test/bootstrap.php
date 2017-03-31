8a67ae821c77313e3c6501b9ce00d6d88be2d9ce
Alexey <f3ath@users.noreply.github.com>
2017-03-30 23:30:16 -0700

    Fixed copyright block (#31)

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,13 +1,11 @@
 <?php
 /**
+ *  This file is part of JSON:API implementation for PHP.
  *
- *  * This file is part of JSON:API implementation for PHP.
- *  *
- *  * (c) Alexey Karapetov <karapetov@gmail.com>
- *  *
- *  * For the full copyright and license information, please view the LICENSE
- *  * file that was distributed with this source code.
+ *  (c) Alexey Karapetov <karapetov@gmail.com>
  *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
