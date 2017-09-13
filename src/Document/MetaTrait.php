2dee705210546c7b68ccade4e27e36630c04de8e
Alexey <f3ath@users.noreply.github.com>
2017-09-12 21:57:53 -0700

    Removed license banner (#59)

--- src/Document/MetaTrait.php
+++ src/Document/MetaTrait.php
@@ -1,13 +1,4 @@
 <?php
-/**
- *  This file is part of JSON:API implementation for PHP.
- *
- *  (c) Alexey Karapetov <karapetov@gmail.com>
- *
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
- */
-
 declare(strict_types=1);
 
 namespace JsonApiPhp\JsonApi\Document;
