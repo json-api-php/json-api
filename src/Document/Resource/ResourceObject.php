e81eb21cfdd92aec3e9b271e50f802a587d76148
Francis Lavoie <lavofr@gmail.com>
2017-10-07 22:00:42 -0400

    Remove whitespace on blank line

--- src/Document/Resource/ResourceObject.php
+++ src/Document/Resource/ResourceObject.php
@@ -95,7 +95,7 @@ class ResourceObject implements \JsonSerializable
     {
         return in_array($name, ['id', 'type']);
     }
-    
+
     private function isValidMemberName(string $name): bool
     {
         return preg_match('/^(?=[^-_ ])[a-zA-Z0-9\x{0080}-\x{FFFF}-_ ]*(?<=[^-_ ])$/u', $name) === 1;
