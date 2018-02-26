8fd9fd02a67e5be95bb96b7882fffadbaae59524
Alexey Karapetov <karapetov@gmail.com>
2018-02-25 21:13:58 -0800

    Naming

--- src/Error.php
+++ src/Error.php
@@ -7,9 +7,9 @@ use JsonApiPhp\JsonApi\Error\ErrorMember;
 
 final class Error extends JsonSerializableValue implements MandatoryErrorDocumentMember
 {
-    public function __construct(ErrorMember ...$errors)
+    public function __construct(ErrorMember ...$members)
     {
-        parent::__construct(combine(...$errors));
+        parent::__construct(combine(...$members));
     }
 
     public function attachTo(object $o)
