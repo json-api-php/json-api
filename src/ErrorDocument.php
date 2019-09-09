5d68025ef111e1d10c05a9fb4bb050a97e5c2120
Sergey Braga <serge.braga@gmail.com>
2019-09-09 19:39:04 +0300

    Remove unnecessary argument Error

--- src/ErrorDocument.php
+++ src/ErrorDocument.php
@@ -8,21 +8,12 @@ use JsonApiPhp\JsonApi\Internal\ErrorDocumentMember;
  * A Document containing an array of Error objects
  * @see http://jsonapi.org/format/#document-top-level
  */
-final class ErrorDocument implements \JsonSerializable
+final class ErrorDocument
 {
-    private $obj;
-
-    public function __construct(Error $error, ErrorDocumentMember ...$members)
+    public function __construct(ErrorDocumentMember ...$members)
     {
-        $this->obj = (object) [];
-        $error->attachTo($this->obj);
         foreach ($members as $member) {
-            $member->attachTo($this->obj);
+            $member->attachTo($this);
         }
-    }
-
-    public function jsonSerialize()
-    {
-        return $this->obj;
     }
 }
